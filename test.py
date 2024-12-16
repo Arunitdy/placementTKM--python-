09a886b0423b75408dc7d121c6c7f7aebb812ddb
Arun <arunmundakkal003@gmail.com>
2024-12-16 11:35:41 +0530

    initial

---
+++ test.py
@@ -0,0 +1,10 @@
+import tkm
+print("hello world")
+print(__name__)
+def main():
+     
+    year=int(input("year of birth:"))
+
+    print("age:",tkm.age(year))
+
+main()
---
+++ tkm.py
@@ -0,0 +1,5 @@
+import datetime as date
+
+def age(year):
+    print(date.datetime.today().year)
+    return date.datetime.today().year-year
