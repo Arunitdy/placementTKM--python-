2a04f39a1f018df15c3a0e4a7c82725d00c681b4
Arun <arunmundakkal003@gmail.com>
2024-12-16 15:53:43 +0530

    //Find the index of largest number in an Array

---
+++ Main.java
@@ -0,0 +1,23 @@
+//Find the index of largest number in an Array
+ class findTheIndexOfLargestNumberInAnArray {
+    
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int[] arr={1,24,34,24,52,94,64,2,23,45,92,91};
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>arr[max]){
+                max=i;
+            }
+            if(arr[arr.length-1-i]>arr[max]){
+                max=i;
+            }
+            
+            System.out.println(i);
+        }
+        System.out.println("number:"+arr[max]+" max; "+max);
+    
+    }
+}
