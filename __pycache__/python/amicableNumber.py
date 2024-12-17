f3d53b2b075299839ae9edcf109c2a49f7ed4bee
Arun <arunmundakkal003@gmail.com>
2024-12-17 14:32:31 +0530

    #print in same line

--- __pycache__/python/amicableNumber.py
+++ __pycache__/python/amicableNumber.py
@@ -1,17 +1,4 @@
-#print in same line
-
-for i in range(1,10):
-    print(10-i,end=" ") # 9 8 7 6 5 4 3 2 1
-print()
-print( 'a', 'b', 'c', sep='-', end = '$')
-print()
-for i in range(1,10):
-    print(i+i-1,end=" ") # 1 3 5 7 9 11 13 15 17
-print()
-
-
 #amicable number (the sum of divisor of A is equal to B and the sum of divisor of B is equal to A)
-
 
 number1=220  #int(input("enter the number1:"))
 
@@ -23,12 +10,21 @@ def sum(num):
         if ( num % i == 0 ):
             sum = sum + i
             
-    print("sum:",sum)
+    #print("sum:",sum)
     return sum
 
 sum1=int(sum(number1))
-sum2=int(sum(number2))
+sum2=int(sum(number2)) 
 
 print(True) if (sum2 == number1 and sum1 == number2) else print(False)
 
-  
+# first  5 amicable number
+
+a = 2  # Start checking from 2 (smallest possible amicable candidate)
+
+print("First 5 Amicable Numbers:")
+while True:
+    b = sum(a)  
+    if b != a and sum(b) == a:  # Check if 'a' and 'b' are amicable
+        print(f"({a}, {b})")
+    a += 1
