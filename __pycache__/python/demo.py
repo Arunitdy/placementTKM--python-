37401922e978aa1fc96ec01c7412bd2e2913d441
Arun <arunmundakkal003@gmail.com>
2024-12-17 10:18:26 +0530

    # 1 3 5 7 9 11 13 15 17

--- __pycache__/python/demo.py
+++ __pycache__/python/demo.py
@@ -1,4 +1,7 @@
 for i in range(1,10):
-    print(10-i,end=" ")
-    
-  
+    print(10-i,end=" ") # 9 8 7 6 5 4 3 2 1
+print()
+print( 'a', 'b', 'c', sep='-', end = '$')
+print()
+for i in range(1,10):
+    print(i+i-1,end=" ") # 1 3 5 7 9 11 13 15 17
