1349072167996f29c5cd9553201aa4bbd13e0f25
Arun <arunmundakkal003@gmail.com>
2025-06-10 14:57:53 +0530

    lngtComnSubse

---
+++ java/lngtComnSubse.java
@@ -0,0 +1,54 @@
+public class lngtComnSubse {
+    static String  text1 = "abcde";
+    static String text2 = "ace";
+
+    public static void displaySubString (int [][] matrix, int i, int j) {
+        if (i == 0 || j == 0) return; 
+        
+        if (text1.charAt(i-1) == text2.charAt(j-1)) {
+            displaySubString(matrix, i-1, j-1);
+            System.out.print(i-1 +":" + text1.charAt(i-1) + " ");
+        } else {
+            if (matrix[i-1][j] < matrix[i][j-1]) {
+                displaySubString(matrix, i, j-1);
+            } else {
+                displaySubString(matrix, i-1, j);
+            }
+        }
+
+    }
+    
+     public static  void display (int [][] matrix,int n,int m) {
+        for (int i = 0; i < n+1; i++) {
+            for (int j = 0; j < m+1; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int [][] matrix = new int[n+1][m+1];
+        for (int i = 0; i < n+1; i++) {
+            for (int j = 0; j < m+1; j++) {
+                if (j == 0 || i == 0) {
+                    matrix[i][j] = 0;
+                } else if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    matrix[i][j] = matrix[i-1][j-1] + 1;
+                } else {
+                    matrix[i][j] = Math.max(matrix[i-1][j], matrix[i][j-1]);
+                }
+
+            }
+        }
+        display(matrix, n, m);
+        displaySubString(matrix, n , m);
+        return matrix[n][m];
+
+    }
+    public static void main(String []args) {
+        System.out.println("\n" + longestCommonSubsequence( text1, text2));
+
+    }
+}
