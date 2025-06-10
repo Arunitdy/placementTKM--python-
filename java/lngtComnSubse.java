860b5b43c8f2ab4ce9530fce299ee84377545826
Arun <arunmundakkal003@gmail.com>
2025-06-10 15:22:02 +0530

    L

--- java/lngtComnSubse.java
+++ java/lngtComnSubse.java
@@ -1,15 +1,18 @@
 public class lngtComnSubse {
-    static String  text1 = "abcde";
-    static String text2 = "ace";
+    static String  text2 = "abcd";
+    static String text1 = "bacd";
 
     public static void displaySubString (int [][] matrix, int i, int j) {
         if (i == 0 || j == 0) return; 
         
         if (text1.charAt(i-1) == text2.charAt(j-1)) {
             displaySubString(matrix, i-1, j-1);
-            System.out.print(i-1 +":" + text1.charAt(i-1) + " ");
+            System.out.println((i - 1) + ":" + (j - 1) + ":" + text1.charAt(i - 1) + " ");
         } else {
-            if (matrix[i-1][j] < matrix[i][j-1]) {
+            if (matrix[i-1][j] == matrix[i][j-1]){
+                displaySubString(matrix, i, j-1);
+                displaySubString(matrix, i-1, j);
+            } else if (matrix[i-1][j] < matrix[i][j-1]) {
                 displaySubString(matrix, i, j-1);
             } else {
                 displaySubString(matrix, i-1, j);
@@ -17,7 +20,7 @@ public class lngtComnSubse {
         }
 
     }
-    
+
      public static  void display (int [][] matrix,int n,int m) {
         for (int i = 0; i < n+1; i++) {
             for (int j = 0; j < m+1; j++) {
@@ -27,9 +30,11 @@ public class lngtComnSubse {
         }
     }
     public static int longestCommonSubsequence(String text1, String text2) {
+
         int n = text1.length();
         int m = text2.length();
         int [][] matrix = new int[n+1][m+1];
+
         for (int i = 0; i < n+1; i++) {
             for (int j = 0; j < m+1; j++) {
                 if (j == 0 || i == 0) {
