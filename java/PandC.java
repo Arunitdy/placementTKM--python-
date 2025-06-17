1cfa2e3f97647700fbb702bbc475943a07c9b4a9
Arun <arunmundakkal003@gmail.com>
2025-06-17 14:46:09 +0530

    findCombinations

--- java/PandC.java
+++ java/PandC.java
@@ -5,28 +5,16 @@ public class PandC {
     // PERMUTATION FUNCTION
     public static void findPermutations(String str, String result) {
         if (str.length() == 0) {
-            System.out.println("Permutation: " + result);
+            System.out.println(result);
             return;
         }
-
         for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            String remaining = str.substring(0, i) + str.substring(i + 1);
-            System.out.println("Current char: " + ch + ", Remaining: " + remaining + ", Result: " + result);
-            findPermutations(remaining, result + ch);
+            findPermutations(str.substring(0,i) + str.substring(i+1), result + str.charAt(i));
         }
     }
 
     // COMBINATION FUNCTION
-    public static void findCombinations(String str, String result, int index) {
-        if (!result.isEmpty()) {
-            System.out.println("Combination: " + result);
-        }
-
-        for (int i = index; i < str.length(); i++) {
-            findCombinations(str, result + str.charAt(i), i + 1);
-        }
-    }
+    
 
     public static void main(String[] args) {
         String input = "ABC";
